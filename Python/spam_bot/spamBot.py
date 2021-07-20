c96dd2cb3cba1885c3234a09b435fbfd8aa9ff1e
Intermarch3 <78302154+Intermarch3@users.noreply.github.com>
2021-07-20 16:02:51 +0200

    Delete spamBot.py

--- Python/spam_bot/spamBot.py
+++
@@ -1,8 +0,0 @@
-import pyautogui
-from time import sleep
-
-sleep(5)  # après avoir lancer le programme, tu a 5 seconde pour clické dans un espace de text et le spam commence
-f = open("txt_spamBot", 'r')  # envoye tout le contenu du fichier txt_spambot
-for word in f:
-    pyautogui.typewrite(word)
-    pyautogui.press("enter")
