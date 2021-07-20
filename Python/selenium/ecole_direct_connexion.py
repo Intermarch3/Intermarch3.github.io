52787337543184517df9392181f971f3cff34a75
Intermarch3 <78302154+Intermarch3@users.noreply.github.com>
2021-07-20 16:02:43 +0200

    Delete ecole_direct_connexion.py

--- Python/selenium/ecole_direct_connexion.py
+++
@@ -1,8 +0,0 @@
-from selenium import webdriver
-
-driver = webdriver.Chrome(executable_path="chromedriver.exe")
-driver.get("https://www.ecoledirecte.com/Eleves/3486/CahierDeTexte")
-
-driver.find_element_by_id("username").send_keys("identifiant ( a marqué )")
-driver.find_element_by_id("password").send_keys("mot de passe ( a marqué )")
-driver.find_element_by_id("connexion").click()
