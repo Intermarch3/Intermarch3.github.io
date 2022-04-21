da59491c68ffdab4b55cc9c6ae6dc04aa3fe0471
Intermarch3 <lucasleclerc74@gmail.com>
2022-04-21 11:50:32 +0200

    add some style

--- web-site/site_4/js/main.js
+++ web-site/site_4/js/main.js
@@ -4,19 +4,22 @@ var quizz = {
             "question": "Quelle est la capitale de la France ?",
             "responses": ["Lyon", "Paris","Marseille"],
             "correct": "Paris",
-            "info": "la capital est Paris et elle se situe au centre de la France"
+            "info": "la capital est Paris et elle se situe au centre de la France",
+            "image": "img/q1.jpg"
         },
         {
             "question": "quelle heure est t'il ?",
             "responses": ["12h", "13h","14h"],
             "correct": "12h",
-            "info": "il est effectivement 12 heure sur ma montre"
+            "info": "il est effectivement 12 heure sur ma montre",
+            "image": "img/q2.jpeg"
         },
         {
             "question": "test question 3 ?",
             "responses": ["1", "2","3"],
             "correct": "3",
-            "info": "fin du test"
+            "info": "fin du test",
+            "image": "img/q3.jpg"
         }
     ]
 }
@@ -30,24 +33,25 @@ var point = 20 / len_quizz
 function play() {
     document.getElementById("menu").style.display = "none"
     next()
-    document.getElementById("main").style.display = "grid"
+    document.getElementById("main").style.display = "block"
 }
 
 function next() {
-    document.getElementsByClassName("score")[0].textContent = Math.round(score) + " / 20"
     nb_question++
     if (nb_question >= quizz.questions.length) {
-        document.getElementsByClassName("nb_questions")[0].textContent = len_quizz + " / " + len_quizz
+        document.getElementById("nb_questions").textContent = len_quizz + " / " + len_quizz
         end()
     } else {
         choisie = false
+        document.getElementsByClassName("next")[0].style.display = "none"
         question = document.getElementsByClassName("question")[0]
         question.innerHTML = "<h1>" + quizz.questions[nb_question].question + "</h1>"
-        document.getElementsByClassName("nb_questions")[0].textContent = nb_question + " / " + len_quizz
+        document.getElementById("nb_questions").textContent = nb_question + " / " + len_quizz
         reponse = document.getElementsByClassName("btn")
         document.getElementById("info_reponse").textContent = ""
+        document.getElementById("img-question").src = quizz.questions[nb_question].image
         for (let i = 0; i < 3; i++) {
-            reponse[i].style.backgroundColor = 'white';
+            reponse[i].style.backgroundColor = '#2a75ff';
             reponse[i].textContent = quizz.questions[nb_question].responses[i]
         }
     }
@@ -60,18 +64,18 @@ function check(elem) {
         if (elem.textContent == quizz.questions[nb_question].correct) {
             console.log("juste")
             score += point
-            document.getElementsByClassName("score")[0].textContent = Math.round(score) + " / 20"
         } else {
             console.log("faux")
         }
         for (let i = 0; i < 3; i++) {
             if (reponse[i].textContent == quizz.questions[nb_question].correct) {
-                reponse[i].style.backgroundColor = 'green';
+                reponse[i].style.backgroundColor = '#009900';
             } else {
-                reponse[i].style.backgroundColor = 'red';
+                reponse[i].style.backgroundColor = '#be0000';
             }
         }
         document.getElementById("info_reponse").textContent = quizz.questions[nb_question].info
+        document.getElementsByClassName("next")[0].style.display = "block"
     }
 }
 
