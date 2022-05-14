e7faf5da1914e42f3bad3a99dbf345124c943326
Intermarch3 <lucasleclerc74@gmail.com>
2022-05-14 16:09:00 +0200

    fix bug

--- js/script.js
+++ js/script.js
@@ -6,38 +6,36 @@ $(".social-btn").hover(
     }
 )
 
+
 function disparition(){
     $(document).ready(function(){
         $(".welcome_page").fadeOut(950, function() {
-            $(".page_general").fadeIn(950)
-            var nodes = document.getElementsByTagName("*");  // gets all elements
-            for(var i=0; i < nodes.length; i++) {
-                nodes[i].style.transition = "all .2s ease-in-out"
-            }
-            var swiper = new Swiper('.swiper-container', {
-                slidesPerView: 1,
-                spaceBetween: 20,
-                effect: 'fade',
-                loop: true,
-                speed: 300,
-                
-                mousewheel: {
-                    invert: false,
-                },
-                pagination: {
-                    el: '.swiper-pagination',
-                    clickable: false,
-                    dynamicBullets: true
-                },
-                autoplay: {
-                    delay: 5000,
-                },
-                // Navigation arrows
-                navigation: {
-                    nextEl: '.swiper-button-next',
-                    prevEl: '.swiper-button-prev',
-                }
-            });
+            $(".page_general").fadeIn(950, function() {
+                var swiper = new Swiper('.swiper-container', {
+                    slidesPerView: 1,
+                    spaceBetween: 20,
+                    effect: 'fade',
+                    loop: true,
+                    speed: 300,
+                    
+                    mousewheel: {
+                        invert: false,
+                    },
+                    pagination: {
+                        el: '.swiper-pagination',
+                        clickable: false,
+                        dynamicBullets: true
+                    },
+                    autoplay: {
+                        delay: 5000,
+                    },
+                    // Navigation arrows
+                    navigation: {
+                        nextEl: '.swiper-button-next',
+                        prevEl: '.swiper-button-prev',
+                    }
+                });
+            })
         })
     })
 }
@@ -68,14 +66,36 @@ document.getElementById('switch_mode').addEventListener('change', (e) => {
 
 
 function light_mode(mode) {
-    nav_bar = document.getElementsByClassName("nav-bar")[0].classList.toggle("light_mode")
     btns = document.getElementsByClassName("nav_bar_btn")
+    bars = document.getElementsByClassName("skill")
+    // add transition effect
+    for (btn of btns) {
+        btn.style.transition = "all .2s ease-in-out"
+    }
+    for (bar of bars) {
+        bar.style.transition = "all .2s ease-in-out"
+    }
+    document.getElementsByClassName("nav-bar")[0].style.transition = "all .2s ease-in-out"
+    document.getElementsByClassName("section1")[0].style.transition = "all .2s ease-in-out"
+    document.getElementsByClassName("h3_card")[0].style.transition = "all .2s ease-in-out"
+    document.getElementsByClassName("h3_card")[1].style.transition = "all .2s ease-in-out"
+    document.getElementsByClassName("wave_sec2")[0].style.transition = "all .2s ease-in-out"
+    document.getElementsByClassName("wave_bas")[0].style.transition = "all .2s ease-in-out"
+    document.getElementsByClassName("slider-image")[5].style.transition = "all .2s ease-in-out"
+    document.getElementsByClassName("intersection")[0].style.transition = "all .2s ease-in-out"
+    document.getElementsByClassName("card")[0].style.transition = "all .2s ease-in-out"
+    document.getElementsByClassName("section3")[0].style.transition = "all .2s ease-in-out"
+    document.getElementsByClassName("title_project")[0].style.transition = "all .2s ease-in-out"
+    document.body.style.transition = "all .2s ease-in-out"
+    document.getElementsByClassName("section4")[0].style.transition = "all .2s ease-in-out"
+    document.getElementsByClassName("button-text")[0].style.transition = "all .2s ease-in-out"
+    // add light_mode class
     for (btn of btns) {
         btn.classList.toggle("light_mode")
     }
-    document.getElementsByClassName("section1")[0].classList.toggle("light_mode")
-    document.getElementsByClassName("h3_card")[0].classList.toggle("light_mode")
-    document.getElementsByClassName("h3_card")[1].classList.toggle("light_mode")
+    for (bar of bars) {
+        bar.classList.toggle("light_mode")
+    }
     if(mode == "light") {
         document.getElementsByClassName("wave_sec2")[0].attributes[1].value = 'img/wave_white_mode.png'
         document.getElementsByClassName("wave_bas")[0].attributes[1].value = 'img/wave_bas_white_mode.png'
@@ -85,21 +105,21 @@ function light_mode(mode) {
         document.getElementsByClassName("wave_bas")[0].attributes[1].value = 'img/wave_bas.png'
         document.getElementsByClassName("slider-image")[5].style.filter = 'invert(0%)'
     }
+    document.body.classList.toggle("light_mode")
+    document.getElementsByClassName("section1")[0].classList.toggle("light_mode")
+    document.getElementsByClassName("h3_card")[0].classList.toggle("light_mode")
+    document.getElementsByClassName("h3_card")[1].classList.toggle("light_mode")
+    document.getElementsByClassName("nav-bar")[0].classList.toggle("light_mode")
     document.getElementsByClassName("intersection")[0].classList.toggle("light_mode")
     document.getElementsByClassName("card")[0].classList.toggle("light_mode")
-    bars = document.getElementsByClassName("skill")
-    for (bar of bars) {
-        bar.classList.toggle("light_mode")
-    }
     document.getElementsByClassName("section3")[0].classList.toggle("light_mode")
     document.getElementsByClassName("title_project")[0].classList.toggle("light_mode")
-    document.body.classList.toggle("light_mode")
     document.getElementsByClassName("section4")[0].classList.toggle("light_mode")
     document.getElementsByClassName("button-text")[0].classList.toggle("light_mode")
 }
 
 
-window.onload=function(){
+window.onload = function() {
     AnimateBtn()
 }
 
@@ -131,7 +151,7 @@ const hideScrollElement = (element) => {
 
 const handleScrollAnimation = () => {
     scrollElements.forEach((el) => {
-        if (elementInView(el, 1.25)) {
+        if (elementInView(el, 1.1)) {
             displayScrollElement(el);
         } else if (elementOutofView(el)) {
             hideScrollElement(el)
